fd023b2f0ffa89b97d402a3debb0892575809dde
hassanalawie <hassan.alawie10@gmail.com>
2020-09-13 16:13:39 -0400

    Updates

---
+++ precache-manifest.cf864891c346cd27a3739d47544d7b6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7732fd6081622fdbd6f25efabca5d0",
+    "url": "/CatchEmAll/index.html"
+  },
+  {
+    "revision": "2a0c7d29231d11ecbd16",
+    "url": "/CatchEmAll/static/css/main.1004b02a.chunk.css"
+  },
+  {
+    "revision": "e55ed680437e28c06a1d",
+    "url": "/CatchEmAll/static/js/2.66198967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CatchEmAll/static/js/2.66198967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0c7d29231d11ecbd16",
+    "url": "/CatchEmAll/static/js/main.10807a95.chunk.js"
+  },
+  {
+    "revision": "d102cc26bd1c77d306d7",
+    "url": "/CatchEmAll/static/js/runtime-main.5a25e51d.js"
+  },
+  {
+    "revision": "b11b15ed73b1f3dea46de9bbe9804870",
+    "url": "/CatchEmAll/static/media/HEADERTITLE.b11b15ed.jpeg"
+  }
+]);
